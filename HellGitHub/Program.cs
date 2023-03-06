65cf74eee3ea4962026fbf317630e34d7a3f4404
daniel.brejla <daniel.brejla@VT1-06>
2023-03-06 10:42:54 +0100

    Přidejte soubory projektu.

---
+++ HellGitHub/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+ Console.WriteLine("ahoj jsem Daniel Brejla,chodím na SŠ Educhem a baví mě ");
+
+string[] zajmy = { "hry hl. FPS", "fotbal", "spánek", "netflix", "" };
+foreach(var item in zajmy)
+{
+Console.WriteLine(item);
+}
